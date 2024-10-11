942784e1599264fd2795809d80150e87b1f4684d
AlianTorv <aliantorv@DESKTOP-KHR02EQ>
2024-10-10 21:34:06 -0400

    player rotation

--- main.py
+++ main.py
@@ -8,10 +8,13 @@ def main():
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        player.update(dt)
         screen.fill(("black"))
         player.draw(screen)
         pygame.display.flip()
